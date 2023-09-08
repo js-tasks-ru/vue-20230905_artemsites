6da1c40d374420eaafe749a78356de4af180c50e
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2023-09-08 14:37:35 +0400

    task done "02-VueCalculator"

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operator: null,
+      result: 0,
+    };
+  },
+  methods: {
+    handlerCalculate() {
+      if (this.operator !== null) {
+        this.result = this.mathIt(this.num1, this.operator, this.num2);
+      }
+    },
+    mathIt(a, operator, b) {
+      let calcFunctions = {
+        '+': () => a + b,
+        '-': () => a - b,
+        '*': () => a * b,
+        '/': () => a / b,
+      };
+      return calcFunctions[operator]();
+    },
+  },
+}).mount('#app');

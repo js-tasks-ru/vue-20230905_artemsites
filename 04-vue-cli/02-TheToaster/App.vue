34ae54a1ab1b3cf56a3e0eff473d24fca01d5de6
artemsites <info@artemsites.ru>
2023-10-29 23:55:05 +0400

    task complete

--- 04-vue-cli/02-TheToaster/App.vue
+++ 04-vue-cli/02-TheToaster/App.vue
@@ -3,6 +3,7 @@
   <p>
     <button type="button" @click="handleSuccessClick">Add Success Message</button>
     <button type="button" @click="handleErrorClick">Add Error Message</button>
+    <button type="button" @click="handleInfoClick">Add Info Message</button>
   </p>
 </template>
 
@@ -21,6 +22,10 @@ export default {
 
     handleErrorClick() {
       this.$refs.toaster.error('Error ' + new Date().toLocaleTimeString());
+    },
+
+    handleInfoClick() {
+      this.$refs.toaster.info('Info ' + new Date().toLocaleTimeString(), 3, true);
     },
   },
 };
